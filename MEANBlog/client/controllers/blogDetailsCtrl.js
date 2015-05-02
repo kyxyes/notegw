ab72a5cd326e324fa63e2818ee572e42f3a166f7
kyxyes <kyxyes@gmail.com>
2015-05-02 00:36:13 -0400

    dele

--- MEANBlog/client/controllers/blogDetailsCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('MEANBlog').controller(function($scope,$http){
-    console.log('hi, this is blogDetails');
-});
--- MEANBlog/client/controllers/displayPostCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('MEANBlog')
-    .controller('displayPostCtrl',function($scope, $http){
-       console.log("hi, this is displayPostCtrl")
-        $http.get('/displayMainPagePosts').success(function(data){
-            $scope.myposts= data;
-        });
-    });
